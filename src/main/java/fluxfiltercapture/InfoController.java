9370cb92dc5c49e9090a53c2e77af9569d91d372
Kleiton Utida <kleiton.utida@live.com>
2023-05-23 21:26:51 -0300

    first commit

---
+++ src/main/java/fluxfiltercapture/InfoController.java
@@ -0,0 +1,15 @@
+package fluxfiltercapture;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class InfoController {
+
+    @PostMapping("/info")
+    public Mono<InfoResponse> getInfo(@RequestBody Info info) {
+        return Mono.just(InfoResponse.builder().success(true).build());
+    }
+}
